535d26f700cda846c0f7021398d5b8d6c89d568f
田村 慧(SB 法人統括) <tamuras44@g.softbank.co.jp>
2025-05-01 15:34:22 +0900

    Update search-vertex-ai.js

--- blocks/search-vertex-ai/search-vertex-ai.js
+++ blocks/search-vertex-ai/search-vertex-ai.js
@@ -1,13 +1,15 @@
 export default async function decorate(block) {
-  block.innerHTML = `
-    <!-- Widget JavaScript bundle -->
-    <script src="https://cloud.google.com/ai/gen-app-builder/client?hl=ja"></script>
-    <!-- Search widget element is not visible by default -->
-    <gen-search-widget
-    configId="f760eeb7-3667-4bc1-99e0-6189313e2ab9"
-    triggerId="searchWidgetTrigger">
-    </gen-search-widget>
-    <!-- Element that opens the widget on click. It does not have to be an input -->
-    <input placeholder="ここで検索" id="searchWidgetTrigger" />
-  `;
+  const script = document.createElement('script');
+  script.src = 'https://cloud.google.com/ai/gen-app-builder/client?hl=ja';
+  block.append(script);
+  script.onload = () => {
+    const genSearchWidget = document.createElement('gen-search-widget');
+    genSearchWidget.configId = 'f760eeb7-3667-4bc1-99e0-6189313e2ab9';
+    genSearchWidget.triggerId = 'searchWidgetTrigger';
+    block.append(genSearchWidget);
+    const input = document.createElement('input');
+    input.placeholder = 'ここで検索';
+    input.id = 'searchWidgetTrigger';
+    block.append(input);
+  };
 }
